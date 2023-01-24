4676ce0e63ae70dc457bd83d5e6831767db816ed
elacymerys <ela.cymerys@gmail.com>
2023-01-24 18:51:33 +0100

    Add program arguments validation

--- src/main/java/org/example/reader/ArgsReader.java
+++ src/main/java/org/example/reader/ArgsReader.java
@@ -9,7 +9,13 @@ public class ArgsReader {
 
         try {
             int floorsNumber = Integer.parseInt(args[0]);
+            if (floorsNumber < 1 || floorsNumber > 10) {
+                throw new IllegalArgumentException("Floors number must be between 1 and 10");
+            }
             int elevatorsNumber = Integer.parseInt(args[1]);
+            if (elevatorsNumber < 1 || elevatorsNumber > 16) {
+                throw new IllegalArgumentException("Elevators number must be between 1 and 16");
+            }
             return new Building(floorsNumber, elevatorsNumber);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Wrong arguments format");
